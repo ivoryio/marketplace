9dbc1627651bc89c34d52581f5f80404f95922d0
MihaiRo <mihai.rosca@thinslices.com>
2019-03-28 12:18:54 +0200

    feat(category nav bar): create an initial category nav bar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -9,38 +9,38 @@ import IconButton from '@ivoryio/kogaio/IconButton'
 import { Flex, Space } from '@ivoryio/kogaio/Responsive'
 
 const Header = ({ transitionToState, user }) => (
-  <TopBar
-    icSize='1.5em'
-    height='56px'
-    alignItems='center'
-    justifyContent='space-between'
-  >
-    <Touchable onClick={transitionToState('landing')}>
-      <Image
-        src={icons.logoLarge}
-        alt='Market placeholder'
-        dimensions={[96, 'auto']}
-      />
-    </Touchable>
-    <Flex>
-      <NavButton icName='shopping_cart' onClick={transitionToState('cart')} />
-      <NavButton
-        icName='account_circle'
-        onClick={transitionToState('profile')}
-      />
-
-      {user ? (
-        <NavButton
-          icName='exit_to_app'
-          onClick={transitionToState('signout')}
+  <TopBar as='header' icSize='1.5em' alignItems='center' p={0} py={2}>
+    <Flex
+      justifyContent='space-between'
+      px={{ xs: '5%', md: '7.5%', lg: '10%' }}
+    >
+      <Touchable onClick={transitionToState('landing')}>
+        <Image
+          src={icons.logoLarge}
+          alt='Market placeholder'
+          dimensions={[96, 'auto']}
         />
-      ) : null}
+      </Touchable>
+      <Flex justifyContent='space-between'>
+        <NavButton icName='shopping_cart' onClick={transitionToState('cart')} />
+        <NavButton
+          icName='account_circle'
+          onClick={transitionToState('profile')}
+        />
+
+        {user ? (
+          <NavButton
+            icName='exit_to_app'
+            onClick={transitionToState('signout')}
+          />
+        ) : null}
+      </Flex>
     </Flex>
   </TopBar>
 )
 
 const NavButton = ({ icName, onClick }) => (
-  <Space p={2}>
+  <Space px={3} py={2}>
     <IconButton
       color='pastel-blue'
       fontSize={4}
