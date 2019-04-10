ff99999bd44e318f76a6b9d3ab9b9fe5839cc20d
Andrei <clim_andrei2000@yahoo.com>
2019-04-10 10:15:13 +0300

    refactor(dynamoDB  and repository): change dynamoDB structure and refactor queryByIds

--- src/pods/catalog/api/src/repository/product.js
+++ src/pods/catalog/api/src/repository/product.js
@@ -65,21 +65,40 @@ function Product () {
   }
 
   const queryByIds = async ids => {
-    let params = {
-      TableName,
-      ScanFilter: {
-        'id': {
-          ComparisonOperator: 'IN',
-          AttributeValueList: ids
-        }
-      }
-    }
+    let params = { RequestItems : {} }
+    let batches = spareInBatches(ids)
     try {
-      let result = await dynamo.scan(params).promise()
-      return result.Items
+      let tasks = batches.map(async batch => {
+        params.RequestItems[`${TableName}`] = { 
+          Keys: batch 
+        }
+
+        let result = await dynamo.batchGet(params).promise()
+        return result
+      })
+
+      let results = await Promise.all(tasks)
+      return results[0].Responses[`${TableName}`]
     } catch (err) {
       throw err
     }
+  }
+
+  const spareInBatches = ids => {
+    const batchSize = 100
+    const numberOfBatches = Math.ceil(ids.length/batchSize)
+    let startPosition = 0
+    let batch = []
+    let batches = []
+
+    for(let i = 0;  i < numberOfBatches; i+=1) {
+      let itemForBatch = ids.slice(startPosition, startPosition + batchSize)
+      batch = itemForBatch.map(id => ({ id } ))
+      batches.push(batch)
+      startPosition += batchSize
+    }
+
+    return batches
   }
 
   return {
@@ -91,3 +110,20 @@ function Product () {
 }
 
 module.exports = new Product()
+
+
+// let params = {
+//   TableName,
+//   ScanFilter: {
+//     'id': {
+//       ComparisonOperator: 'IN',
+//       AttributeValueList: ids
+//     }
+//   }
+// }
+// try {
+//   let result = await dynamo.scan(params).promise()
+//   return result.Items
+// } catch (err) {
+//   throw err
+// }
