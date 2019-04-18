896645e1b17cddafcb7c73b49d0625d99f854b84
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-04-18 15:09:39 +0300

    feat(stream controller): change controller to handle remove events

--- src/pods/catalog/api/src/dbStreamController.js
+++ src/pods/catalog/api/src/dbStreamController.js
@@ -10,10 +10,11 @@ const { unmarshall } = AWS.DynamoDB.Converter
 exports.handler = async (event, context) => {
   const endpoints = await retrieveSecret(process.env.SEARCH_HOSTNAME_SECRET)
   const { docService } = JSON.parse(endpoints)
+  const { eventName } = event.Records[0]
 
   const cloudSearchDomain = new AWS.CloudSearchDomain({ endpoint: docService })
 
-  if (event.Records[0].eventName === 'INSERT') {
+  if (eventName === 'INSERT') {
     const item = unmarshall(event.Records[0].dynamodb.NewImage)
 
     const fieldsToInsert = normalizeFields(item)
@@ -30,5 +31,19 @@ exports.handler = async (event, context) => {
     }
 
     await cloudSearchDomain.uploadDocuments(insertParams).promise()
+  } else if(eventName === 'REMOVE') {
+    const { id } = unmarshall(event.Records[0].dynamodb.OldImage)
+
+    const documentToRemove = [{
+      type: 'delete',
+      id
+    }]
+
+    const removeParams = {
+      contentType: 'application/json',
+      documents: JSON.stringify(documentToRemove)
+    }
+    
+    await cloudSearchDomain.uploadDocuments(removeParams).promise()    
   }
 }
