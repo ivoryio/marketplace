46c513495c7e38144053e58c018d1293478101ac
IoanaVas <ioana.dvasiliu@yahoo.com>
2019-04-18 10:42:15 +0300

    feat(catalog api): add stream controller for dynamo
    contain method to get cloudseach indexes

--- src/pods/catalog/api/src/dbStreamController.js
+++ src/pods/catalog/api/src/dbStreamController.js
@@ -1,3 +1,24 @@
-exports.handler = (event, context) => {
+const AWS = require('aws-sdk')
 
+// const retrieveSecret = require('../src/services/retrieveSecret')
+
+// const { unmarshall } = AWS.DynamoDB.Converter
+
+exports.handler = async (event, context) => {
+
+  const cloudSearch = new AWS.CloudSearch()
+
+
+  if (event.Records[0].eventName === 'INSERT') {
+    // const item = unmarshall(event.Records[0].dynamodb.NewImage)
+
+    const indexParams = { DomainName: 'catalog-search' }
+    const indexes = await cloudSearch.describeIndexFields(indexParams).promise()
+    const indexNames = indexes.IndexFields.map(index => index.Options.IndexFieldName)
+
+    return indexNames
+
+
+
+  }
 }
