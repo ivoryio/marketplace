202210ae88b63bcd24debbc15b8224bd39c137d1
msal96 <mihailsalari1996@yahoo.com>
2019-05-07 13:09:39 +0300

    style(catalog.dataservice): replace && with &

--- src/pods/catalog/web/services/catalog.dataservice.js
+++ src/pods/catalog/web/services/catalog.dataservice.js
@@ -1,7 +1,7 @@
 import { API } from 'aws-amplify'
 
 const getNewestProducts = () =>
-  API.get('catalog', '/products?sortBy=createdat.desc&&limit=12', {
+  API.get('catalog', '/products?sortBy=createdat.desc&limit=12', {
     response: true
   })
 
@@ -9,7 +9,7 @@ const getSearchResults = searchTerm =>
   API.get('catalog', `/products?q=${searchTerm}`, { response: true })
 
 const getSpotlightWatches = () =>
-  API.get('catalog', 'products?isspotlight=true&&limit=9', {
+  API.get('catalog', 'products?isspotlight=true&sortBy=createdat.desc&limit=9', {
     response: true
   })
 
