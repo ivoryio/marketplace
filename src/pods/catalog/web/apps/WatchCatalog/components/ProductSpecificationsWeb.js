552a62668826694faba4326bacad50e2314cf4fa
msal96 <mihailsalari1996@yahoo.com>
2019-05-13 11:26:10 +0300

    chore(ProductsSpecificationsWeb): Add description

--- src/pods/catalog/web/apps/WatchCatalog/components/ProductSpecificationsWeb.js
+++ src/pods/catalog/web/apps/WatchCatalog/components/ProductSpecificationsWeb.js
@@ -14,10 +14,20 @@ const options = [
     value: '48h'
   }
 ]
+const longDescription = 'WatchShopping.com buys and sells brand new prestigous watches from all around the world. All our products are inspected and purchased directly from countries all around the world to provide the lowest cost, yet, best service quality for our customers. We sell brands such as Rolex, Cartier, Omega, Tudor, Vacheron Constantin, Audemars Piguet, Patek Philippe, Officine Panerai, Hublot and more. If you have a specific watch brand and model that you are looking for, please feel free to talk to us and we will assist you to search for it. Please feel free to talk to us for any questions or concerns. '
 
 const ProductSpecificationsWeb = (props) => (
   <Flex flexDirection='column' {...props}>
-    <Typography color='pastel-blue' fontSize={0}>Caliber</Typography>
+    <Typography color='gunmetal' fontSize={3} fontWeight={0}>Details</Typography>
+    <Space mt={2}>
+      <Typography lineHeight='26px' color='gunmetal' fontSize={1} fontWeight={0}>{longDescription}</Typography>
+    </Space>
+    <Space mt={5}>
+      <Typography color='gunmetal' fontSize={3} fontWeight={0}>Info & Stats</Typography>
+    </Space>
+    <Space mt={2}>
+      <Typography color='pastel-blue' fontSize={0}>Caliber</Typography>
+    </Space>
     <Space mt={2}>
       <InfoTable options={options} />
     </Space>
