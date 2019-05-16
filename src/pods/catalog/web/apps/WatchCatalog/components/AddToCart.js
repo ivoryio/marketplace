a9fb5a0802f90c04e0cebcae0cf2c426511e480c
msal96 <mihailsalari1996@yahoo.com>
2019-05-16 17:08:41 +0300

    fix(Spotlight, newest sections, catalog entry): Fix infinite requests

--- src/pods/catalog/web/apps/WatchCatalog/components/AddToCart.js
+++ src/pods/catalog/web/apps/WatchCatalog/components/AddToCart.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Box, Button, Card, Flex, Hide, Icon, Space, themeGet, Touchable, Typography } from '@ivoryio/kogaio'
 
+import { NavigationContext } from '../WatchCatalogEntry'
 import { formatPrice } from '../services/helpers'
 
-const AddToCart = ({ brand, model, price, ...props }) => {
+const AddToCart = ({ ...props }) => {
   const [quantity, setQuantity] = useState(1)
 
+  const { watchDetails: {
+    data: {
+      brand,
+      model,
+      price
+    }
+  } } = useContext(NavigationContext)
+
   const _decrementQuantity = () => {
-    if(quantity !== 1) {
+    if (quantity !== 1) {
       setQuantity(quantity - 1)
     }
   }
@@ -19,115 +28,114 @@ const AddToCart = ({ brand, model, price, ...props }) => {
     element.scrollIntoView({ behavior: "smooth" })
   }
 
-  const anchors = [{ title: 'Details', scrollingId: 'details' }, {title: 'Info & Stats', scrollingId: 'info&stats'}]
+  const anchors = [{ title: 'Details', scrollingId: 'details' }, { title: 'Info & Stats', scrollingId: 'info&stats' }]
   const formattedPrice = `$${formatPrice(Number(price))}`
   return (
     <Box position={{ xs: 'sticky', lg: 'block' }} bottom={0} width={{ xs: 1, lg: 4 / 10 }}>
-            <Space pt={4} pb={{ xs: 4, lg: 0 }}>
-              <Card
-                zIndex={2}
-                width={1}
-                position={{ xs: 'block', lg: 'sticky' }}
-                top={0}
-                bg='white'
-                boxShadow='card-simple'
-                display='flex'
-                justifyContent='center' 
+      <Space pt={4} pb={{ xs: 4, lg: 0 }}>
+        <Card
+          zIndex={2}
+          width={1}
+          position={{ xs: 'block', lg: 'sticky' }}
+          top={0}
+          bg='white'
+          boxShadow='card-simple'
+          display='flex'
+          justifyContent='center'
+        >
+          <Flex
+            flexDirection='column'
+            width={{ xs: 1, md: 7 / 10, lg: 1 }}
+          >
+            <Space px={{ xs: 6, md: 0, lg: 4 }}>
+              <WatchTitle
+                color='gunmetal'
+                fontSize={4}
               >
-                <Flex
-                  flexDirection='column'
-                  width={{ xs: 1, md: 7 / 10, lg: 1 }}
-                >
-                  <Space px={{ xs: 6, md: 0, lg: 4 }}>
-                    <WatchTitle
-                      color='gunmetal'
-                      fontSize={4}
-                    >
-                      {brand} {model}
-                    </WatchTitle>
-                  </Space>
-                  <Space mt={6} px={{ xs: 6, md: 0, lg: 6 }}>
-                    <Flex
-                      alignItems='center'
-                      justifyContent='space-between'
-                    >
-                      <Typography
-                        color='gunmetal'
-                        fontSize={4}
-                        fontWeight={2}
-                      >
-                        {formattedPrice}
-                      </Typography>
-                      <Flex width={{ xs: 4 / 10, md: 1 / 4, lg: 4 / 10 }}>
-                        <Touchable
-                          width={3 / 10}
-                          effect='highlight'
-                          onClick={_decrementQuantity}
-                        >
-                          <QuantityModifierWrapper
-                            alignItems='center'
-                            justifyContent='center'
-                            
-                            height='36px'
-                          >
-                            <Typography>-</Typography>
-                          </QuantityModifierWrapper>
-                        </Touchable>
-                        <QuantityWrapper
-                          alignItems='center'
-                          justifyContent='center'
-                          width={4 / 10}
-                          height='36px'
-                        >
-                          {quantity}
-                        </QuantityWrapper>
-                        <Touchable
-                          effect='highlight'
-                          onClick={() => setQuantity(prevState => prevState + 1)}
-                          width={3 / 10}
-                        >
-                          <QuantityModifierWrapper
-                            height='36px'
-                            alignItems='center'
-                            justifyContent='center'
-                          >
-                            <Typography>+</Typography>
-                          </QuantityModifierWrapper>
-                        </Touchable>
-                      </Flex>
-                    </Flex>
-                  </Space>
-                  <Space mt={5} px={{ xs: 6, md: 0, lg: 6 }}>
-                    <Box width={1}>
-                      <Button
-                        width={1}
-                        title='ADD TO MY CART'
-                        onClick={() => {}}
-                      />
-                    </Box>
-                  </Space>
-                  <Hide xs sm md>
-                    {
-                      anchors.map(({ title, scrollingId }, index) => (
-                        <Space key={`anchor-to-${scrollingId}`} mt={index === 0 ? 6 : 0}>
-                          <Touchable effect='opacity' onClick={scrollTo(scrollingId)}>
-                            <Space px={6} py={4}>
-                              <WebScrollingItem alignItems='center' width={1}>
-                                <Typography>{title}</Typography>
-                                <Space ml={1}>
-                                  <Icon name='link' />
-                                </Space>
-                              </WebScrollingItem>
-                            </Space>
-                          </Touchable>
-                        </Space>
-                      ))
-                    }
-                  </Hide>
-                </Flex>
-              </Card>
+                {brand} {model}
+              </WatchTitle>
             </Space>
-            </Box>
+            <Space mt={6} px={{ xs: 6, md: 0, lg: 6 }}>
+              <Flex
+                alignItems='center'
+                justifyContent='space-between'
+              >
+                <Typography
+                  color='gunmetal'
+                  fontSize={4}
+                  fontWeight={2}
+                >
+                  {formattedPrice}
+                </Typography>
+                <Flex width={{ xs: 4 / 10, md: 1 / 4, lg: 4 / 10 }}>
+                  <Touchable
+                    width={3 / 10}
+                    effect='highlight'
+                    onClick={_decrementQuantity}
+                  >
+                    <QuantityModifierWrapper
+                      alignItems='center'
+                      justifyContent='center'
+                      height='36px'
+                    >
+                      <Typography>-</Typography>
+                    </QuantityModifierWrapper>
+                  </Touchable>
+                  <QuantityWrapper
+                    alignItems='center'
+                    justifyContent='center'
+                    width={4 / 10}
+                    height='36px'
+                  >
+                    {quantity}
+                  </QuantityWrapper>
+                  <Touchable
+                    effect='highlight'
+                    onClick={() => setQuantity(prevState => prevState + 1)}
+                    width={3 / 10}
+                  >
+                    <QuantityModifierWrapper
+                      height='36px'
+                      alignItems='center'
+                      justifyContent='center'
+                    >
+                      <Typography>+</Typography>
+                    </QuantityModifierWrapper>
+                  </Touchable>
+                </Flex>
+              </Flex>
+            </Space>
+            <Space mt={5} px={{ xs: 6, md: 0, lg: 6 }}>
+              <Box width={1}>
+                <Button
+                  width={1}
+                  title='ADD TO MY CART'
+                  onClick={() => { }}
+                />
+              </Box>
+            </Space>
+            <Hide xs sm md>
+              {
+                anchors.map(({ title, scrollingId }, index) => (
+                  <Space key={`anchor-to-${scrollingId}`} mt={index === 0 ? 6 : 0}>
+                    <Touchable effect='opacity' onClick={scrollTo(scrollingId)}>
+                      <Space px={6} py={4}>
+                        <WebScrollingItem alignItems='center' width={1}>
+                          <Typography>{title}</Typography>
+                          <Space ml={1}>
+                            <Icon name='link' />
+                          </Space>
+                        </WebScrollingItem>
+                      </Space>
+                    </Touchable>
+                  </Space>
+                ))
+              }
+            </Hide>
+          </Flex>
+        </Card>
+      </Space>
+    </Box>
   )
 }
 
--- src/pods/catalog/web/apps/WatchCatalog/screens/WatchDetails.js
+++ src/pods/catalog/web/apps/WatchCatalog/screens/WatchDetails.js
@@ -13,9 +13,6 @@ const WatchDetails = () => {
     setActiveWatchId,
     watchDetails: {
       data: {
-        brand,
-        model,
-        price,
         imgList
       },
       isFetching
@@ -39,10 +36,11 @@ const WatchDetails = () => {
       />
     </Space>
   }
+
   return (
     <Flex flexDirection='column' alignItems='center'>
       <Flex width={1}>
-        <Space ml={{ xs:1, md: 2, lg: 4 }} mt={{ xs: 2, md: 3, lg: 4 }}>
+        <Space ml={{ xs: 1, md: 2, lg: 4 }} mt={{ xs: 2, md: 3, lg: 4 }}>
           <BackButton onClick={goBack} />
         </Space>
       </Flex>
@@ -51,31 +49,27 @@ const WatchDetails = () => {
           <Flex flexWrap='wrap' width={{ xs: 1, lg: 6 / 10 }}>
             <Gallery imgList={imgList} />
             <Hide lg>
-            <Space px={4} mt={6}>
-              <ProductSpecificationsMobile width={1} />
-            </Space>
-          </Hide>
-          <Hide xs sm md>
+              <Space px={4} mt={6}>
+                <ProductSpecificationsMobile width={1} />
+              </Space>
+            </Hide>
+            <Hide xs sm md>
               <Space px={4} mt={2}>
                 <ProductSpecificationsWeb width={1} />
               </Space>
               <Flex width={1 / 4} />
-          </Hide>
-        </Flex>
-        <Space mt={{ xs: 2, lg: 0 }}>
-          <AddToCart
-            brand={brand}
-            model={model}
-            price={price}
-          />
-        </Space>
+            </Hide>
+          </Flex>
+          <Space mt={{ xs: 2, lg: 0 }}>
+            <AddToCart />
+          </Space>
         </Flex>
       </Space>
-        <Space py={{ xs: 6, lg: 15  }} mt={10}>
-          <NewestWatchesWrapper bg='ghost-white' width={1}>
-            <Region name='newest-watches' />
-          </NewestWatchesWrapper>
-        </Space>
+      <Space py={{ xs: 6, lg: 15 }} mt={10}>
+        <NewestWatchesWrapper bg='ghost-white' width={1}>
+          <Region name='newest-watches' />
+        </NewestWatchesWrapper>
+      </Space>
     </Flex>
   )
 }
@@ -85,4 +79,3 @@ const NewestWatchesWrapper = styled(Box)`
 `
 
 export default WatchDetails
-
