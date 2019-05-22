e955cfdcaa3907cfb13b12d6d3900559fcc49502
MihaiRo <mihai.rosca@thinslices.com>
2019-05-22 14:10:18 +0300

    (fix: loading): add spotlight cards loading

--- src/pods/catalog/web/apps/SpotlightWatches/SpotlightWatches.js
+++ src/pods/catalog/web/apps/SpotlightWatches/SpotlightWatches.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import Carousel from '@brainhubeu/react-carousel'
-import {
-  ActivityIndicator,
-  Box,
-  Flex,
-  Hide,
-  Space,
-  Typography
-} from '@ivoryio/kogaio'
+import { Box, Flex, Hide, Space, Typography } from '@ivoryio/kogaio'
 
 import { Arrow, CardWatch } from '../../components'
 import '@brainhubeu/react-carousel/lib/style.css'
 import api from '../../services/catalog.dataservice'
 
+const isResponseOk = statusCode => statusCode >= 200 && statusCode < 300
 const groupCards = arr => {
   const length = arr.length
   let index = 0
@@ -32,8 +26,8 @@ const mapGroupsToSlides = arr =>
       flexWrap='wrap'
       justifyContent='center'
       key={first.id}
-      width={1}
-    >
+      pb={1}
+      width={1}>
       <Space mt={3} px={{ xs: 2, lg: 3 }}>
         <Box width={{ xs: 1, md: 3 / 5, lg: 1 / 2 }}>
           <CardWatch
@@ -98,7 +92,7 @@ const SpotlightWatches = () => {
     const fetchWatches = async () => {
       try {
         const response = await api.getSpotlightWatches()
-        if (response.status === 200) {
+        if (isResponseOk(response.status)) {
           const groups = groupCards(response.data.items)
           const slides = mapGroupsToSlides(groups)
           setSpotlightWatches({ data: slides, isFetching: false })
@@ -134,13 +128,13 @@ const SpotlightWatches = () => {
           Subtitle with a call to action label goes here
         </Typography>
       </Space>
-      <Space px={2} mt={5}>
-        <Flex justifyContent='center' width={{ xs: 1, md: 1, lg: 3 / 4 }}>
-          <StyledCarousel
+      <Space mt={4}>
+        <Flex justifyContent='center' width={1}>
+          <SpotlightCarousel
             addArrowClickHandler
             arrowLeft={<Arrow direction='left' />}
             arrowRight={<Arrow direction='right' />}
-            infinite={true}
+            infinite
             value={activeElement}
             onChange={setActiveElement}
             slidesPerPage={1}
@@ -169,29 +163,59 @@ const SpotlightWatches = () => {
                 clickToChange: false,
                 animationSpeed: 2000
               }
-            }}
-          >
+            }}>
             {isFetching ? (
-              <ActivityIndicator
-                colors={{ background: 'white', primary: 'gunmetal' }}
-                size='32px'
-              />
+              <LoadingPlaceholder />
             ) : (
               data.map(cardWithWatches => cardWithWatches)
             )}
-          </StyledCarousel>
+          </SpotlightCarousel>
         </Flex>
       </Space>
     </Flex>
   )
 }
 
-const StyledCarousel = styled(Carousel)`
+const SpotlightCarousel = styled(Carousel)`
   width: 100%;
-
-  .BrainhubCarouselItem div {
-    margin-bottom: 4px;
-  }
 `
+
+const LoadingPlaceholder = () => (
+  <Flex alignItems='center' flexWrap='wrap' width={1}>
+    <Space mt={3} px={{ xs: 2, lg: 3 }}>
+      <Box width={{ xs: 1, md: 3 / 5, lg: 1 / 2 }}>
+        <Box
+          bg='ice-white'
+          borderRadius={3}
+          height={{ xs: '322px', md: '374px' }}
+          maxHeight='374px'
+          width={1}
+        />
+      </Box>
+    </Space>
+    <Space mt={3} px={{ xs: 2, lg: 3 }}>
+      <Box width={{ xs: 1 / 2, md: 2 / 5, lg: 1 / 4 }}>
+        <Box
+          bg='ice-white'
+          borderRadius={3}
+          height={{ xs: '322px', md: '374px' }}
+          width={1}
+        />
+      </Box>
+    </Space>
+    <Hide md>
+      <Space mt={3} px={{ xs: 2, lg: 3 }}>
+        <Box width={{ xs: 1 / 2, lg: 1 / 4 }}>
+          <Box
+            bg='ice-white'
+            borderRadius={3}
+            height={{ xs: '322px', md: '374px' }}
+            width={1}
+          />
+        </Box>
+      </Space>
+    </Hide>
+  </Flex>
+)
 
 export default SpotlightWatches
